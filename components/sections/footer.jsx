cff8a6bb0f9dc666399b56394b9e748610c6bcb2
haroldmud <91553966+haroldmud@users.noreply.github.com>
2026-06-09 15:44:26 +0200

    feat: make fundamental imporovements

--- components/sections/footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer(props){
-  return(
-    <footer id="footer" className={`bg-blue w-full mt-20 py-12 ${props.style}`}>
-      <section  className="max-w-7xl w-6/12 mx-auto text-blue">
-        <p className="text-gray-400 font-[300] text-center mt-4">©2023 BETON-CLEF TECHNIQUE Construction Company, Inc. All Rights Reserved</p>
-      </section>
-      </footer>
-  )
-}
