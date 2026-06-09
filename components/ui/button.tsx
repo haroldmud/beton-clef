5c4ecdc26198c575844540e1bcdfde400064a66c
haroldmud <91553966+haroldmud@users.noreply.github.com>
2026-06-09 16:33:46 +0200

    implementing the roboto font

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -4,13 +4,12 @@ interface ButtonProps {
   children: ReactNode
   style?: string
   onClick?: () => void
-  href?: string
 }
 
-export default function Button({ children, style, onClick, href }: ButtonProps) {
+export default function Button({ children, style, onClick }: ButtonProps) {
   return (
     <button
-      className={`md:flex flex-col justify-center- hover:shadow-xl bg-yellow sm:rounded-lg rounded-xl text-blue md:text-xl text-base font-bold sm:p-1 sm:px-6 p-2 shadow-sm ${style ?? ''}`}
+      className={`md:flex flex-col justify-center hover:shadow-xl bg-yellow sm:rounded-lg rounded-xl text-blue md:text-xl text-base font-bold sm:p-1 sm:px-6 p-2 shadow-sm ${style ?? ''}`}
       onClick={onClick}
     >
       {children}
