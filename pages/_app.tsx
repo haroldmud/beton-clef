1b740c750a49decadd872d58f1fd57822524a994
haroldmud <91553966+haroldmud@users.noreply.github.com>
2026-06-09 15:38:12 +0200

    implementing the roboto font

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
+}
