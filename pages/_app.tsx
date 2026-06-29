e27b3cf3d52c6d41e55343684ba81d54c04cc7d0
Arnold Mudosa <arnold.mudosa@Arnolds-MacBook-Pro.local>
2026-06-29 18:45:59 +0200

    feat: fix the header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   return (
-    <main>
+    <main key={router.pathname} className="page-fade">
       <Component {...pageProps} />
     </main>
   )
